38d44aaa7acdcf3a69e7cd22bc3d7b04017c4778
Marcus Alexio PRADO <mgprado1@my.waketech.edu>
2025-02-19 13:54:42 -0500

    Started on css file

---
+++ zzz.js
@@ -0,0 +1,10 @@
+
+//Making new buttons
+let newButton = document.createElement('button')
+newButton.id = 'but4' //create id
+newButton.innerHTML = 'Choice 4' //text inside
+document.body.appendChild(newButton) //add it into html
+
+// Removing buttons
+//let rem = document.getElementById('but1') //makes variable
+//rem.remove() //removes it
