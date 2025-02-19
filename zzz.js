7e4f87389da3fa1d621210e2c9685eb202c79656
Marcus Alexio PRADO <mgprado1@my.waketech.edu>
2025-02-19 18:02:39 -0500

    started character description

--- zzz.js
+++ zzz.js
@@ -8,3 +8,24 @@ document.body.appendChild(newButton) //add it into html
 // Removing buttons
 //let rem = document.getElementById('but1') //makes variable
 //rem.remove() //removes it
+
+
+//Character selection
+
+let corin = document.getElementById('charImage1');
+let lycaon = document.getElementById('charImage2');
+let rina = document.getElementById('charImage3');
+const charName = document.getElementById('placeholder')
+const charDesc = document.getElementById('charText')
+
+corin.onclick = function() {
+    charName.textContent = 'Corin';
+}
+lycaon.onclick = function() {
+    charName.textContent = 'Lycaon';
+
+}
+rina.onclick = function() {
+    charName.textContent = 'Rina';
+
+}
